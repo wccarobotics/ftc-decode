77e94a7b96d1a55882f624b43a9b10479fe416de
Lucas Plaisted <lsplaisted@outlook.com>
2025-11-20 08:38:16 -0500

    Add pinpoint odometry code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/PinpiontOdometry.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.GoBildaPinpointDriver;
+
+public class PinpiontOdometry {
+    GoBildaPinpointDriver odo;
+    public void init(HardwareMap hardwareMap){
+        odo = hardwareMap.get(GoBildaPinpointDriver.class,"odo");
+        odo.setOffsets(-59.1, -120.7, DistanceUnit.MM);
+        odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED, GoBildaPinpointDriver.EncoderDirection.REVERSED);
+        odo.resetPosAndIMU();
+    }
+    public double getX(){
+        odo.update();
+        Pose2D pos = odo.getPosition();
+        return pos.getX(DistanceUnit.MM);
+    }
+    public double getY(){
+        odo.update();
+        Pose2D pos = odo.getPosition();
+        return pos.getY(DistanceUnit.MM);
+    }
+    public double getHeading(){
+        odo.update();
+        Pose2D pos = odo.getPosition();
+        return pos.getHeading(AngleUnit.DEGREES);
+    }
+    public void resetImu(){
+        odo.recalibrateIMU();
+    }
+    public void resetEverything(){
+        odo.resetPosAndIMU();
+    }
+}
