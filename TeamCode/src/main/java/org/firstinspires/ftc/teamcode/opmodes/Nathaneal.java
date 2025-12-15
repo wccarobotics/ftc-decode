45bb5e1d5e128df41925c9c772ec7f0a874fe109
Lucas Plaisted <lsplaisted@outlook.com>
2025-12-15 18:25:48 -0500

    Nathaneal learning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Nathaneal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Nathaneal.java
@@ -7,14 +7,21 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class Nathaneal extends OpMode {
     @Override
     public void init() {
-        String myWorld = "World";
+        String myWorld = "the best number is probably ";
+        double theNumber = 67;
 
-        telemetry.addData("Hello", myWorld);
+        telemetry.addData("oh", myWorld+theNumber);
     }
 
     @Override
-    public void loop() {
-
+    public void loop()
+        { double LS = -gamepad1.left_stick_y * 2.0;
+            telemetry.addData("Left stick y", gamepad1.left_stick_y);
+            telemetry.addData("B Button", gamepad1.b);
+            telemetry.addData("X Button", gamepad1.x);
+            telemetry.addData("Y Button", gamepad1.y);
+            telemetry.addData("A Button", gamepad1.a);
+            telemetry.addData("LS*2.0", LS );
 
     }
 }
