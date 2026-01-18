dd90fd5ee2cb5cef8653813122585b8a4f65eaf9
Lucas Plaisted <lsplaisted@outlook.com>
2026-01-18 12:33:47 -0500

    Add launcher test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp
+public class test extends OpMode{
+    private DcMotorEx launcher;
+    double power;
+    @Override
+    public void init(){
+        launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        power = 500;
+    }
+    @Override
+    public void loop(){
+        if (gamepad1.dpadUpWasPressed()){
+            power += 50;
+        }
+        else if (gamepad1.dpadDownWasPressed()) {
+            power -= 50;
+        }
+        else if (gamepad1.b) {
+            power = 0;
+        }
+        else if (gamepad1.a)
+        {
+            power = 500;
+        }
+        launcher.setVelocity(power / 60 * 28);
+        telemetry.addData("Target RPM", power);
+        telemetry.addData("RPM ", (launcher.getVelocity()) * 60 / 28);
+    }
+}
