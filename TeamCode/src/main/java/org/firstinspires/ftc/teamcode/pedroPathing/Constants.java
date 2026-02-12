28e1a75317c8b05dea487c14c1153f6bcfbff3f9
Lucas Plaisted <lsplaisted@outlook.com>
2026-02-12 09:20:59 -0500

    Pedro pathing tuning (WIP)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -16,7 +18,11 @@ public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(11.43)
             .forwardZeroPowerAcceleration(-33.0471836058062)
-            .lateralZeroPowerAcceleration(-52.56124850604056);
+            .lateralZeroPowerAcceleration(-52.56124850604056)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.2, 0, 0.02, 0.03))
+            .headingPIDFCoefficients(new PIDFCoefficients(2,0,0.1,0.02))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients())
+            ;
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
